de56006e7cf7bd990406b53cda47b3dbc83bcc65
Ali Arifin <103297661+ali-arifin@users.noreply.github.com>
2025-12-18 06:44:25 +0500

    Create while-loop.sh

---
+++ loops/while-loop.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+start=1
+while [[ $start -le 10 ]]
+do
+  echo "Hello World : $start"
+  ((start++))
+done
+
+
+
+#!/bin/bash
+start=10
+while [[ $start -ge 1 ]]
+do
+  echo "Alnafi : $start"
+  ((start--))
+done
+
+
+
+#!/bin/bash
+file="name.txt"
+while read each_line
+do
+  echo "$each_line"
+done < $file
+
+
+
+#!/bin/bash
+file="/root/bash/ali/name.txt"
+cat $file | while read each_line        # prints line by line
+do
+  echo "$each_line"
+done
+
+
+
+#!/bin/bash
+ls -ltrh  | while read each_line
+do
+  echo "$each_line"
+done
+
+
+
+#!/bin/bash
+file="/root/bash/ali/server.csv"
+cat $file | while IFS="," read var1 var2 var3
+do
+  echo "$var3"
+done
+
+
+
+#!/bin/bash
+file="/root/bash/ali/server1.csv"
+while IFS="," read var1 var2 var3
+do
+  echo "$var3"
+done < $file

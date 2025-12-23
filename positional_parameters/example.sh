1d3a846c81918997818c05db87ecb2788b09e60f
Ali Arifin <103297661+ali-arifin@users.noreply.github.com>
2025-12-23 14:54:38 +0500

    Update example.sh

--- positional_parameters/example.sh
+++ positional_parameters/example.sh
@@ -2,11 +2,11 @@
 echo "This is positional parameter example script" $0
 echo "Hello, My name is : " $1
 echo "Here is " $2
-echo "this is " $3
+echo "This is " $3
 echo "total count value is " $#
-echo "Display the parameter the passed" $*
-echo "Fetch information" $@
-echo "PID of current shell"
+echo "Display the passed parameter" $*
+echo "Display the passed parameter" $@
+echo "PID of current shell" $$
 echo "Exit status of last executed command " $?
 
 
@@ -16,7 +16,7 @@ echo "Exit status of last executed command " $?
 $0 --> Script name
 $1..$9 --> parameter passed
 $# --> Count the total number of parameters
-$*  --> Display the parameter passed
+$*  --> Display the passed parameter
 $@ same as $* both are same
 $$ --> current PID of shell
 '
